1f7b7e65ae790228be6964318cccbcab4c99bb5b
PareekSuvidhi <pareek.suvidhi@gmail.com>
2025-07-13 17:47:31 +0530

    Add files via upload

---
+++ 01_Basics/01JavaBasics1.java
@@ -0,0 +1,81 @@
+public class JavaBasics1 {
+    public static void main(String args[]){
+        // Output in java
+        // System.out.println("Hello World ");
+        // System.out.println("Hello Earth ");
+        // System.out.print("Hello India");
+        // System.out.println(" Hello Singapore");
+        // System.out.print("Suvidhi\n");
+        // System.out.println("Pareek");
+        // System.out.print("Vishal\nPareek");
+
+        // Printing a pattern
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+
+// // Variables in Java
+//             int a = 10;
+//             int b = 5;
+//             System.out.println("a");  // Anything written in the doule quote gets printed as it is.
+//             System.out.println(a);
+//             System.out.println(b);
+//             String name = "Tony Stark";
+//             System.out.println(name);
+
+
+//     // After writing some lines of code if we want to change value of any variable let say for 'a' 
+//             a = 50;
+//             System.out.println(a);
+
+//     // We can assign value of one variable to another. Here, value of 'b' is assigned to 'a'
+//             a = b;
+//             System.out.println(a);
+//             System.out.println(b);
+
+
+// // Data types in java
+//             System.out.println("Data types in JAVA");
+//             byte by = 8;
+//             System.out.println(by);
+
+//             char ch = '@';
+//             System.out.println(ch);
+//             System.out.println("ch");
+
+//             boolean var = true;
+//             boolean var1 = false;
+
+//             int number = 800;
+//             System.out.println(number);
+
+//             long number2 = 900;
+//             System.out.println(number2);
+
+//             float price = 10.5F;
+//     //why to specify f or F because bydefault java consider decimal value as double which cannot be implicitly assigned to float because float has lower precision (32-bit) than double (64-bit).
+//             System.out.println(price);
+
+//             double price2 = 10.12;
+//             System.out.println(price2);
+            
+//             short n = 76;
+//             System.out.println(n);
+
+//             int x = 2147483647;
+//             System.out.println(x);
+
+            long y = 2147483648L;
+//             System.out.println(y);
+
+
+// // Sum of a and b
+
+//             int p = 10;
+//             int s = 15;
+//             int sum = p + s;
+//             System.out.println(sum);
+
+    }
+}
