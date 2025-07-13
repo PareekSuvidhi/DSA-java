222e046fd3fbe82c8840923350a609f56dc52886
PareekSuvidhi <pareek.suvidhi@gmail.com>
2025-07-13 17:53:52 +0530

    Add files via upload

---
+++ 02_Operators/09UnaryOperators9.java
@@ -0,0 +1,29 @@
+public class UnaryOperators9 {
+    public static void main(String args[]){
+
+        // INCREMENT OPERATOR
+        // int a = 10;
+        // int b = ++a;           // Pre increment -> a ki value change -> a ki value use
+        // System.out.println(a);  
+        // System.out.println(b);
+
+        // int a = 10;
+        // int b = a++;          // Post increment -> a ki previous value use -> a ki value change
+        // System.out.println(a);  
+        // System.out.println(b);
+
+        
+        // DECREMENT OPERATOR
+        // int a = 10;
+        // int b = --a;          // Pre decrement
+        // System.out.println(a);
+        // System.out.println(b);
+
+        int a = 10;
+        int b = a--;            // Post decrement
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+}
---
+++ 02_Operators/12AssignmentOperator12.java
@@ -0,0 +1,34 @@
+public class AssignmentOperator12 {
+    public static void main(String args[]) {
+
+        int Z = 12;
+        Z = 7;
+        System.out.println(Z);
+
+        // int A = 10;
+        // A += 10;      // A = A + 10;
+        // System.out.println(A);
+
+
+        // int B = 5;
+        // B -= 5;       // B = B - 5;
+        // System.out.println(B);
+
+
+        // int C = 15;
+        // C *= 4;      // C = C * 4;
+        // System.out.println(C);
+
+
+        // int D = 6;
+        // D /= 6;        // D = D / 6;
+        // System.out.println(D);
+
+
+        int E = 4;
+        E %= 2;     // E = E % 2;
+        System.out.println(E);
+
+
+    }
+}
